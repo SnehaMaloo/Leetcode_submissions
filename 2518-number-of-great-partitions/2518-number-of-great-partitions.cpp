6f7707dd079426f1a8ba00bd5f1e7e0d427509c6
Sneha Maloo <107611558+SnehaMaloo@users.noreply.github.com>
2022-12-26 21:26:29 +0530

    Time: 149 ms (75.00%), Space: 66.2 MB (58.33%) - LeetHub

---
+++ 2518-number-of-great-partitions/2518-number-of-great-partitions.cpp
@@ -0,0 +1,53 @@
+#define ll long long 
+class Solution {
+public:
+    int mod=1e9+7;
+    ll power(ll a, ll b, ll m)
+{
+	if (b == 0)
+		return 1;
+	ll x = power(a, b / 2, m);
+	x = (x * x) % m;
+	if (b % 2)
+	{
+		x = (x * a) % m;
+	}
+	return x;
+}
+   int solve(int ind,vector<int>&nums,int k,vector<vector<long long>> &dp){
+
+    if(ind==nums.size()){
+            return 1;
+    }
+    
+    long long ans=0;
+    if(dp[ind][k]!=-1)
+        return dp[ind][k];
+    
+    if( k>nums[ind])
+        ans+=solve(ind+1,nums,k-nums[ind],dp)%mod;
+    
+    ans+=solve(ind+1,nums,k,dp)%mod;
+    
+    return dp[ind][k]=ans%mod;
+}
+public:
+int countPartitions(vector<int>& nums, int k) {
+int n=nums.size();
+ll sum=0;
+for(auto i:nums){
+sum+=i;
+}
+
+      if(2*k>sum)
+         return 0;
+
+    vector<vector<long long>> dp(n,vector<long long>(k+1,-1));
+    ll val=solve(0,nums,k,dp);
+    ll totalval=power(2,n,mod);   
+
+    totalval-=(2*val);
+    totalval=(totalval%mod+mod)%mod;
+    return totalval;
+}
+};
