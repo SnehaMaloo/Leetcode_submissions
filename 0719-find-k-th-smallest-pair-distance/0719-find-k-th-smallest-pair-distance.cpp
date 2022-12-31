349f96e58d04e7b18883dafa8d6d3ff7b638f637
Sneha Maloo <107611558+SnehaMaloo@users.noreply.github.com>
2022-12-31 15:09:59 +0530

    Time: 8 ms (98.09%), Space: 10.2 MB (12.29%) - LeetHub

---
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int smallestDistancePair(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int low=0,high=nums[n-1]-nums[0];
+        while(low<high)
+        {
+            int mid=low+(high-low)/2;
+            int cnt=0;
+            for(int i=0,j=0;i<n;i++)
+            {
+                while(j<n&&(nums[j]-nums[i])<=mid)
+                {
+                    j++;
+                }
+                cnt+=j-i-1;
+            }
+            if(cnt<k)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid;
+            }
+        }
+        return low;
+    }
+};
