354ecf6825e5ccca3a857c9b84e14743b2b63a79
Sneha Maloo <107611558+SnehaMaloo@users.noreply.github.com>
2023-01-08 23:29:11 +0530

    Time: 5 ms (46.74%), Space: 7.5 MB (22.81%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
-    int dp[1002][1002][2];
-    int solve(int idx,int prev,vector<int>&nums,bool ok)
-    {
-        if(idx==nums.size())
-        {
-            return 0;
-        }
-        if(dp[idx][prev+1][ok]!=-1)
-        {
-            return dp[idx][prev+1][ok];
-        }
-        if(prev==-1)
-        {
-            return dp[idx][prev+1][ok]=max(solve(idx+1,idx,nums,ok),solve(idx+1,prev,nums,ok));
-        }
-        else if(ok)
-        {
-            if((nums[idx]-nums[prev])>0)
-            {
-                return dp[idx][prev+1][ok]=max(1+solve(idx+1,idx,nums,!ok),solve(idx+1,prev,nums,ok));
-            }
-            return dp[idx][prev+1][ok]=solve(idx+1,prev,nums,ok);
-        }
-        else
-        {
-             if((nums[idx]-nums[prev])<0)
-            {
-                return dp[idx][prev+1][ok]=max(1+solve(idx+1,idx,nums,!ok),solve(idx+1,prev,nums,ok));
-            }
-            return dp[idx][prev+1][ok]=solve(idx+1,prev,nums,ok);
-        }
-    }
     int wiggleMaxLength(vector<int>& nums) {
         int n=nums.size();
-        int prev=-1;
-        memset(dp,-1,sizeof(dp));
-        return max(1+solve(0,prev,nums,true),1+solve(0,prev,nums,false));
+        vector<int>up(n,1),down(n,1);
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i-1]>nums[i])
+            {
+                up[i]=down[i-1]+1;
+                down[i]=down[i-1];
+            }
+            else if(nums[i]>nums[i-1])
+            {
+                down[i]=up[i-1]+1;
+                up[i]=up[i-1];
+            }
+            else
+            {
+                up[i]=up[i-1];
+                down[i]=down[i-1];
+            }
+        }
+        return max(up[n-1],down[n-1]);
     }
 };
