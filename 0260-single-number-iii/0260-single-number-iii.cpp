482e7ea2141e122814dce6cf7cb3064d9596d37c
Sneha Maloo <107611558+SnehaMaloo@users.noreply.github.com>
2023-01-11 02:02:46 +0530

    Time: 9 ms (87.10%), Space: 9.9 MB (94.45%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         int n=nums.size();
-        int xor_0=0;
+        int xor0=0;
+        int i;
+        for(i=0;i<n;i++)
+        {
+            xor0^=nums[i];
+        }
+        int xor1=0,xor2=0;
+        int k=0;
+        while((xor0&(1<<k))==0)
+        {
+            k++;
+        }
         for(int i=0;i<n;i++)
         {
-            xor_0^=nums[i];
-        }
-        int i=0;
-        while((xor_0&(1<<i))==0)
-        {
-            i++;
-        }
-        int xor_1=0,xor_2=0;
-        for(int j=0;j<n;j++)
-        {
-            if(nums[j]&(1<<(i)))
+            if(nums[i]&(1<<(k)))
             {
-                xor_1^=nums[j];
+                xor1^=nums[i];
             }
             else
             {
-                xor_2^=nums[j];
+                xor2^=nums[i];
             }
         }
-        vector<int>res({xor_1,xor_2});
+        vector<int>res({xor1,xor2});
         return res;
     }
 };
