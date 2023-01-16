6d1932a4a8b1e27e8eefba125f1dc202a020de52
Sneha Maloo <107611558+SnehaMaloo@users.noreply.github.com>
2023-01-16 19:53:59 +0530

    Time: 35 ms (34.36%), Space: 17.1 MB (78.65%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -11,38 +11,19 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& ivt, vector<int>& nvt) {
         vector<vector<int>>res;
         int n=ivt.size();
+        ivt.push_back(nvt);
         sort(ivt.begin(),ivt.end(),compare);
-        bool flag=false;
-        if(n==0)
+        res.push_back(ivt[0]);
+        for(int i=1;i<=n;i++)
         {
-            flag=true;
-            res.push_back(nvt);
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(!flag&&nvt[1]<ivt[i][0])
+            if(ivt[i][0]<=res[res.size()-1][1])
             {
-                res.push_back(nvt);
-                flag=true;
+                res[res.size()-1][1]=max(res[res.size()-1][1],ivt[i][1]);
             }
-            if(i!=0&&ivt[i][0]<=res[res.size()-1][1])
+            else
             {
-                res[res.size()-1][0]=min(ivt[i][0],res[res.size()-1][0]);
-                res[res.size()-1][1]=max(ivt[i][1],res[res.size()-1][1]);
-            }
-            else{
                 res.push_back(ivt[i]);
             }
-            if(!flag&&nvt[0]<=res[res.size()-1][1])
-            {
-                 res[res.size()-1][0]=min(nvt[0],res[res.size()-1][0]);
-                res[res.size()-1][1]=max(nvt[1],res[res.size()-1][1]);
-                flag=true;
-            }
-        }
-        if(!flag)
-        {
-            res.push_back(nvt);
         }
         return res;
     }
