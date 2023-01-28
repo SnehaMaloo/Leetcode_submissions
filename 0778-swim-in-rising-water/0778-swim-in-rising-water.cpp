20731da95c7bc34f63d8702e4099a4d734989712
Sneha Maloo <107611558+SnehaMaloo@users.noreply.github.com>
2023-01-28 16:17:50 +0530

    Time: 22 ms (74.52%), Space: 9 MB (76.43%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,1e9));
+        int dx[4]={1,0,0,-1};
+        int dy[4]={0,1,-1,0};
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>q;
+        q.push({grid[0][0],{0,0}});
+        vis[0][0]=grid[0][0];
+        while(!q.empty())
+        {
+            int t=q.top().first;
+            int x=q.top().second.first;
+            int y=q.top().second.second;
+            q.pop();
+            if(x==n-1&&y==n-1)
+            {
+                return t;
+            }
+            for(int i=0;i<4;i++)
+            {
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+                if(nx<n&&ny<m&&nx>=0&&ny>=0&&vis[nx][ny]>t)
+                {
+                    vis[nx][ny]=t;
+                    q.push({max(t,grid[nx][ny]),{nx,ny}});
+                }
+            }
+        }
+        return -1;
+    }
+};
