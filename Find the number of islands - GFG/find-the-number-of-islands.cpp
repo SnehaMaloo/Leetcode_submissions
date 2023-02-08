b4f3361470401c1c64734b1b659c5ba09d324d77
Sneha Maloo <107611558+SnehaMaloo@users.noreply.github.com>
2023-02-09 03:50:09 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -6,46 +6,34 @@ using namespace std;
 class Solution {
   public:
     // Function to find the number of islands.
-    bool ischeck(int nx,int ny,int n,int m)
+    void dfs(int x,int y,vector<vector<char>>&grid)
     {
-        if(nx>=0&&nx<n&&ny>=0&&ny<m)
+        grid[x][y]='&';
+        int n=grid.size();
+        int m=grid[0].size();
+        int dx[8]={-1,-1,-1,0,0,1,1,1};
+        int dy[8]={-1,0,1,-1,1,-1,0,1};
+        for(int i=0;i<8;i++)
         {
-            return true;
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+            if(nx>=0&&nx<n&&ny>=0&&ny<=m&&grid[nx][ny]=='1')
+            {
+                dfs(nx,ny,grid);
+            }
         }
-        return false;
     }
     int numIslands(vector<vector<char>>& grid) {
-        int i,j;
         int n=grid.size(),m=grid[0].size();
-        int dx[8]={1,1,-1,-1,0,0,1,-1};
-        int dy[8]={1,-1,-1,1,1,-1,0,0};
         int cnt=0;
-        for(i=0;i<n;i++)
+        for(int i=0;i<n;i++)
         {
-            for(j=0;j<m;j++)
+            for(int j=0;j<m;j++)
             {
                 if(grid[i][j]=='1')
                 {
                     cnt++;
-                    queue<pair<int,int>>q;
-                    q.push({i,j});
-                    while(!q.empty())
-                    {
-                        int x=q.front().first;
-                        int y=q.front().second;
-                        grid[x][y]='&';
-                        q.pop();
-                        for(int k=0;k<8;k++)
-                        {
-                            int nx=x+dx[k];
-                            int ny=y+dy[k];
-                            if(ischeck(nx,ny,n,m)&&grid[nx][ny]=='1')
-                            {
-                                q.push({nx,ny});
-                                grid[nx][ny]='&';
-                            }
-                        }
-                    }
+                    dfs(i,j,grid);
                 }
             }
         }
