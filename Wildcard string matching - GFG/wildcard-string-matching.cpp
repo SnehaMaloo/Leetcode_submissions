eccb00d78933deaa3d708d11621e9b0a3ceefd00
Sneha Maloo <107611558+SnehaMaloo@users.noreply.github.com>
2023-08-14 19:04:03 +0530

    Added solution - LeetHub

--- Wildcard string matching - GFG/wildcard-string-matching.cpp
+++ Wildcard string matching - GFG/wildcard-string-matching.cpp
@@ -10,35 +10,37 @@ class Solution{
     {
         int n=wild.size();
         int m=pattern.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,false));
-        dp[0][0]=true;
+        vector<bool>dp(m+1,false);
+        dp[0]=true;
         for(int j=0;j<n;j++)
         {
            if(wild[j]!='*')
            {
                break;
            }
-            dp[j][0]=true;
+            dp[j]=true;
         }
         for(int i=1;i<=n;i++)
         {
+            vector<bool>curr(m+1,false);
             for(int j=1;j<=m;j++)
             {
                 if(wild[i-1]==pattern[j-1])
                 {
-                    dp[i][j]=dp[i-1][j-1];
+                    curr[j]=dp[j-1];
                 }
                 else if(wild[i-1]=='?')
                 {
-                   dp[i][j]=dp[i-1][j-1];
+                   curr[j]=dp[j-1];
                 }
                 else if(wild[i-1]=='*')
                 {
-                    dp[i][j]=dp[i-1][j-1]||dp[i][j-1]||dp[i-1][j];
+                    curr[j]=dp[j-1]||curr[j-1]||dp[j];
                 }
             }
+            dp=curr;
         }
-        return dp[n][m];
+        return dp[m];
     }
 };
 
